532c51f2bada24f69b015bc167d93e4d76a8a544
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-16 12:24:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,12 @@ def line(input)
   if input.length == 0
     puts "The line is currently empty."
   else
-    print "The line is currently: "
+    print "The line is currently:"
     alength = input.size
     num = 0
     num1 = 1
     while num < alength
-      print "#{num1}. #{input[num]}"
+      print " #{num1}. #{input[num]}"
       num += 1
       num1 += 1
     end
